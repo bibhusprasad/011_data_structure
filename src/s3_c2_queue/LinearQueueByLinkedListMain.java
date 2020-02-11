724485cd25d74e5177bfaa2376436e28c4455e0e
Bibhu Prasad Samal <bibhusprasad@gmail.com>
2020-02-11 20:08:50 +0530

    DS: Linear Queue using Linked list

---
+++ src/s3_c2_queue/LinearQueueByLinkedListMain.java
@@ -0,0 +1,24 @@
+package s3_c2_queue;
+
+public class LinearQueueByLinkedListMain {
+
+    public static void main(final String[] args) {
+        System.out.println("Creating a queue");
+        final LinearQueueByLinkedList queue = new LinearQueueByLinkedList();
+
+        System.out.println("Enqueueing 6 values into queue");
+        for (int i = 1; i <= 6; i++) {
+            queue.enQueue(i * 10);
+        }
+        System.out.println();
+
+        System.out.println("Peeking value");
+        System.out.println(queue.peek());
+        System.out.println();
+
+        System.out.println("Dequeueing 6 values into queue");
+        for (int i = 0; i < 6; i++) {
+            System.out.println(queue.deQueue());
+        }
+    }
+}
